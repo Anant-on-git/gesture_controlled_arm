1acc47405668d6912df28ecbb9a85d19d123dc51
Anant-on-git <kishorsumbpatil7588@gmail.com>
2026-03-21 02:48:47 +0530

    added other commands to the launch file

--- arm_ws/src/imu_reader/launch/arm_launch.py
+++ arm_ws/src/imu_reader/launch/arm_launch.py
@@ -1,14 +1,34 @@
+from pathlib import Path
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    urdf_path = Path("/home/anant/gesture_controlled_arm/arm_ws/model/arm.urdf")
+
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             parameters=[{
-                'robot_description': open('/home/anant/gesture_controlled_arm/arm_ws/model/arm.urdf').read()
+                'robot_description': urdf_path.read_text()
             }]
+        ),
+        Node(
+            package='imu_reader',
+            executable='imu_reader',
+            output='screen',
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['0', '0', '0', '0', '0', '0', 'map', 'base_link'],
+            output='screen',
         )
     ])
