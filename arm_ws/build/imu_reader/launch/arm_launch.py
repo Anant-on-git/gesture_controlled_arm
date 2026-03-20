cf3ea0005f2ddbe2a8efef1c6e2cb85e7d7bfeb4
Anant-on-git <kishorsumbpatil7588@gmail.com>
2026-03-20 21:37:16 +0530

    adding the arm model and a launch file

---
+++ arm_ws/build/imu_reader/launch/arm_launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{
+                'robot_description': open('/home/anant/gesture_controlled_arm/arm_ws/model/arm.urdf').read()
+            }]
+        )
+    ])
